15d27e0b7831e0b3f3c6f05dd7b85749db5cec9e
gungho0619 <tzztntt@gmail.com>
2024-10-21 14:14:49 +1000

    add: infoPromotion

---
+++ config/promotion.js
@@ -0,0 +1,20 @@
+// config.js
+const longLifeCoinPromotion = `
+🔷 *What is LongLifeCoin?*
+
+The main purpose is to help others because helping others is the foundation of happiness. We aim to help patients alleviate their suffering and benefit humanity.
+
+LongLifeCoin is a cryptocurrency that supports the poor and those in need by providing access to Chinese herbal medicine and free clinics.
+
+Our goal is to build free clinics or small hospitals where people can get treatments using 5-vegetable juice or 6-vegetable juice, a traditional Chinese medicine method.
+
+📢 Visit: LongLifeCoin.com to learn more about this project.
+
+We aim to gather $1,000,000 for this mission with 10^12 tokens priced at $1.5 each. Your support can help solve homelessness in America and improve the quality of life for many.
+
+💬 Use /info to learn more.
+`;
+
+module.exports = {
+  longLifeCoinPromotion,
+};
